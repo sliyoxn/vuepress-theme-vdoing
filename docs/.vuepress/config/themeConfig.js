5472b001f3afafccf124268e581bbe501b33a50b
sliyoxn <43871064+sliyoxn@users.noreply.github.com>
2021-09-24 17:59:40 +0800

    config: 修改配置

--- docs/.vuepress/config/themeConfig.js
+++ docs/.vuepress/config/themeConfig.js
@@ -7,7 +7,7 @@ module.exports = {
   nav,
   sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
   logo: '/img/EB-logo.png', // 导航栏logo
-  repo: 'xugaoyi/vuepress-theme-vdoing', // 导航栏右侧生成Github链接
+  repo: 'https://github.com/sliyoxn', // 导航栏右侧生成Github链接
   searchMaxSuggestions: 10, // 搜索结果显示最大数
   lastUpdated: '上次更新', // 开启更新时间，并配置前缀文字   string | boolean (取值为git提交时间)
   docsDir: 'docs', // 编辑的文件夹
@@ -47,39 +47,39 @@ module.exports = {
 
   author: {
     // 文章默认的作者信息，可在md文件中单独配置此信息 String | {name: String, link: String}
-    name: 'xugaoyi', // 必需
-    link: 'https://github.com/xugaoyi', // 可选的
+    name: 'SakuraSnow', // 必需
+    link: 'https://github.com/sliyoxn', // 可选的
   },
   blogger: {
     // 博主信息，显示在首页侧边栏
-    avatar: 'https://cdn.jsdelivr.net/gh/xugaoyi/image_store/blog/20200103123203.jpg',
-    name: 'Evan Xu',
-    slogan: '前端界的小学生',
+    avatar: 'https://sakurasnow-knowledge-library.oss-cn-shenzhen.aliyuncs.com/library-img/header.jpeg',
+    name: 'SakuraSnow',
+    slogan: '一只前端咸鱼',
   },
   social: {
     // 社交图标，显示于博主信息栏和页脚栏
     // iconfontCssFile: '//at.alicdn.com/t/font_1678482_u4nrnp8xp6g.css', // 可选，阿里图标库在线css文件地址，对于主题没有的图标可自由添加
-    icons: [
-      {
-        iconClass: 'icon-youjian',
-        title: '发邮件',
-        link: 'mailto:894072666@qq.com',
-      },
-      {
-        iconClass: 'icon-github',
-        title: 'GitHub',
-        link: 'https://github.com/xugaoyi',
-      },
-      {
-        iconClass: 'icon-erji',
-        title: '听音乐',
-        link: 'https://music.163.com/#/playlist?id=755597173',
-      },
-    ],
+    // icons: [
+    //   {
+    //     iconClass: 'icon-youjian',
+    //     title: '发邮件',
+    //     link: 'mailto:894072666@qq.com',
+    //   },
+    //   {
+    //     iconClass: 'icon-github',
+    //     title: 'GitHub',
+    //     link: 'https://github.com/xugaoyi',
+    //   },
+    //   {
+    //     iconClass: 'icon-erji',
+    //     title: '听音乐',
+    //     link: 'https://music.163.com/#/playlist?id=755597173',
+    //   },
+    // ],
   },
   footer: {
     // 页脚信息
-    createYear: 2019, // 博客创建年份
+    createYear: 2021, // 博客创建年份
     copyrightInfo:
       'Evan Xu | <a href="https://github.com/xugaoyi/vuepress-theme-vdoing/blob/master/LICENSE" target="_blank">MIT License</a>', // 博客版权信息，支持a标签
   },
