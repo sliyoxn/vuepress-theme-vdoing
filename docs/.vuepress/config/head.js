f1728e83539eb883267a192f18bfb617ae1114c8
sliyoxn <43871064+sliyoxn@users.noreply.github.com>
2021-09-25 13:51:22 +0800

    config

--- docs/.vuepress/config/head.js
+++ docs/.vuepress/config/head.js
@@ -1,9 +1,13 @@
 // head
+// 图标和header相关
 module.exports = [
   // 注入到页面<head> 中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
-  ['link', { rel: 'icon', href: '/img/favicon.ico' }], //favicons，资源放在public文件夹
+  ['link', { rel: 'icon', href: '/img/sakura-snow_favicon.ico' }], //favicons，资源放在public文件夹
   [
-    'meta',
+
+
+
+
     {
       name: 'keywords',
       content: '前端博客,个人技术博客,前端,前端开发,前端框架,web前端,前端面试题,技术文档,学习,面试,JavaScript,js,ES6,TypeScript,vue,python,css3,html5,Node,git,github,markdown',
--- docs/.vuepress/config/themeConfig.js
+++ docs/.vuepress/config/themeConfig.js
@@ -1,18 +1,18 @@
 const nav = require('./nav.js');
 const htmlModules = require('./htmlModules.js');
 // const sidebar = require('./sidebar.js');
-
+const avatar = 'https://sakurasnow-knowledge-library.oss-cn-shenzhen.aliyuncs.com/library-img/header.jpeg'
 // 主题配置
 module.exports = {
   nav,
   sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
-  logo: '/img/EB-logo.png', // 导航栏logo
+  logo: avatar, // 导航栏logo
   repo: 'https://github.com/sliyoxn', // 导航栏右侧生成Github链接
   searchMaxSuggestions: 10, // 搜索结果显示最大数
   lastUpdated: '上次更新', // 开启更新时间，并配置前缀文字   string | boolean (取值为git提交时间)
   docsDir: 'docs', // 编辑的文件夹
-  editLinks: true, // 启用编辑
-  editLinkText: '编辑',
+  // editLinks: true, // 启用编辑
+  // editLinkText: '编辑',
 
   //*** 以下配置是Vdoing主题改动和新增的配置 ***//
 
@@ -52,7 +52,7 @@ module.exports = {
   },
   blogger: {
     // 博主信息，显示在首页侧边栏
-    avatar: 'https://sakurasnow-knowledge-library.oss-cn-shenzhen.aliyuncs.com/library-img/header.jpeg',
+    avatar: avatar,
     name: 'SakuraSnow',
     slogan: '一只前端咸鱼',
   },
